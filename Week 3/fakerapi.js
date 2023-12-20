28ae089b2ffb993b6b4235cc1204bd1fc50e5ea9
Sohail Gidwani <sohailgidwani15@gmail.com>
2023-12-20 12:24:32 +0530

    making the fakerapi info cleaner

--- Week 3/fakerapi.js
+++ Week 3/fakerapi.js
@@ -11,18 +11,18 @@ async function getUserData(){
     const finalData = await res.json();
     console.log(finalData);
     document.getElementById("user-data-container").innerHTML = JSON.stringify(finalData.data);
-    // const container = document.getElementById('user-data-container');
-    // container.innerHTML = ''; 
+    const container = document.getElementById('user-data-container');
+    container.innerHTML = ''; 
 
-    // finalData.data.forEach(item => {
-    //     const userDiv = document.createElement('div');
-    //     userDiv.innerHTML = `
-    //         <p>Name: ${item.firstname} ${item.lastname}</p>
-    //         <p>Email: ${item.email}</p>
-    //         <p>Phone: ${item.phone}</p>
-    //         <p>Birthday: ${item.birthday}</p>
-    //         <hr>
-    //     `;
-    //     container.appendChild(userDiv);
-    // });
+    finalData.data.forEach(item => {
+        const userDiv = document.createElement('div');
+        userDiv.innerHTML = `
+            <p>Name: ${item.firstname} ${item.lastname}</p>
+            <p>Email: ${item.email}</p>
+            <p>Phone: ${item.phone}</p>
+            <p>Birthday: ${item.birthday}</p>
+            <hr>
+        `;
+        container.appendChild(userDiv);
+    });
 }
